8b42b24305c06d27b9ffa55be816b399c8931478
jereycui2008 <jeremycuid@gmail.com>
2023-09-03 11:29:25 -0700

    On encryption/decryption.

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,26 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
+	public static void main(String[] args) {
+		
+	
+	try {
 	// Create a program that takes a message from the user and saves it to a file.
+	String message = JOptionPane.showInputDialog("Enter a message.");
+	
+	FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/test2.txt", true);
+	fw.write(message);
+	fw.close();
+	
+	
+} catch (IOException e) {
+	e.printStackTrace();
+}
+
+}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,10 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +25,26 @@ public class FileEncryptor {
 	 * Create a program that takes a message and a key from the user.
 	 * Use the key to shift each letter in the users input and save the final result to a file.
 	 */
+	public static void main(String[] args) {
+		
+		
+	try {
+	// Create a program that takes a message from the user and saves it to a file.
+		String message = JOptionPane.showInputDialog("Enter a message.");
+		String key = JOptionPane.showInputDialog("Enter a key.");
+	
+	FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/encryption.txt");
+	for(int i = 0; i<message.length(); i++) {
+		
+	}
+	fw.write(message);
+	fw.close();
+	
+	
+} catch (IOException e) {
+	e.printStackTrace();
+}
+
+}
+	
 }
